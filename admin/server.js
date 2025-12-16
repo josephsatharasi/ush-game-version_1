2eb94841ba82b7e1ed36ce0d1465114ea5e30886
JosephSubmittech <joseph@submittech.com>
2025-12-16 08:08:30 +0530

    Fix catch-all route

--- admin/server.js
+++ admin/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing - return index.html for all routes
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
